5d384be2fb68ada803c2c9e28d321e5ef92bf71c
mtrahan <mtrahan@github.com>
2023-11-06 15:00:23 -0700

    Added CharlieCloud support.

--- bin/parflow.sh
+++ bin/parflow.sh
@@ -9,6 +9,17 @@ then
 elif singularity version >& /dev/null
 then
    singularity run docker://parflow/parflow:latest $*
+elif ch-checkns >& /dev/null
+then
+   if [ ! -d $(pwd)/.parflow_image ]
+   then
+       echo "running: ch-image pull parflow/parflow"
+       ch-image pull parflow/parflow
+       echo "running: ch-convert parflow/parflow $(pwd)/.parflow_image"
+       ch-convert parflow/parflow $(pwd)/.parflow_image
+   fi
+   echo "running: ch-run -w -b $(pwd):/data -c /data --set-env $(pwd)/.parflow_image -- pfrun $*"
+   ch-run -w -b $(pwd):/data -c /data --set-env $(pwd)/.parflow_image -- pfrun $*
 else
-   echo "Couldn't run podman, docker or singularity"
+   echo "Couldn't run podman, docker, charliecloud, or singularity"
 fi
